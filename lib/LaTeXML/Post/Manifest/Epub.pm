e94fd108d0a7347eee8ae557822562fdf589e7b3
Deyan Ginev <d.ginev@jacobs-university.de>
2013-10-19 17:07:22 +0000

    first stab at Manifest post-processors (still in dev)

---
+++ lib/LaTeXML/Post/Manifest/Epub.pm
@@ -0,0 +1,58 @@
+# /=====================================================================\ #
+# |  LaTeXML::Post::Manifest                                            | #
+# | Manifest creation for EPUB                                          | #
+# |=====================================================================| #
+# | Part of LaTeXML:                                                    | #
+# |  Public domain software, produced as part of work done by the       | #
+# |  United States Government & not subject to copyright in the US.     | #
+# |---------------------------------------------------------------------| #
+# | Bruce Miller <bruce.miller@nist.gov>                        #_#     | #
+# | http://dlmf.nist.gov/LaTeXML/                              (o o)    | #
+# \=========================================================ooo==U==ooo=/ #
+package LaTeXML::Post::Manifest::Epub;
+use strict;
+use warnings; 
+
+use base qw(LaTeXML::Post::Manifest);
+use LaTeXML::Util::Pathname;
+use File::Spec::Functions qw(catdir);
+
+our $container_content = <<'EOL';
+<container version="1.0">
+  <rootfiles>
+    <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+  </rootfiles>
+</container>
+EOL
+
+sub process {
+  my($self,$doc,$root)=@_;
+  return $doc if $self->{finished};
+  # Run a single time, even if there are multiple document fragments
+  $self->{finished} = 1;
+
+  my $directory = $self->{siteDirectory};
+
+  # 1. Create mimetype declaration
+  open my $epub_fh, ">", pathname_concat($directory,'mimetype');
+  print $epub_fh 'application/epub+zip';
+  close $epub_fh;
+  # 2. Create META-INF metadata directory
+  my $meta_inf_dir = catdir($directory,'META-INF');
+  mkdir $meta_inf_dir;
+  # 2.1. Add the container.xml description
+  open my $container_fh, ">". pathname_concat($meta_inf_dir,'container.xml');
+  print $container_fh $container_content;
+  close $container_fh;
+
+  # 3. Create OEBPS content container
+  my $OEBPS_dir = catdir($directory,'OEBPS');
+  mkdir $OEBPS_dir;
+  # 3.1 OEBPS/content.opf XML Spine
+  # 3.2 OEBPS/toc.ncx XML ToC
+  # 3.3 OEBPS/Text - XHTML files
+  # 3.4. Images??? Others?
+  return $doc;
+}
+
+1;
